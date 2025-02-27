097a2cce6c725a50d461fc967c65d1520496855e
DanGonite57 <13329420+DanGonite57@users.noreply.github.com>
2025-02-27 16:13:27 +0000

    change keybind to ctrl

--- src/napari_threedee/utils/mouse_callbacks.py
+++ src/napari_threedee/utils/mouse_callbacks.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+
 from napari.layers.utils.interactivity_utils import drag_data_to_projected_distance
 
 from napari_threedee.utils.napari_utils import add_point_on_plane
@@ -19,7 +20,7 @@ def on_mouse_alt_click_add_point_on_plane(
         replace_selected: bool = False,
 ):
     # Early exit if not alt-clicked
-    if 'Alt' not in event.modifiers:
+    if 'Ctrl' not in event.modifiers:
         return
 
     add_point_on_plane(
@@ -28,4 +29,3 @@ def on_mouse_alt_click_add_point_on_plane(
         image_layer=image_layer,
         replace_selected=replace_selected
     )
-
